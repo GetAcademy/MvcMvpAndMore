781c8947ea2ee8d28379b93caa7e898ab303d5da
GET Academy <terje@getacademy.no>
2022-06-21 09:13:47 +0200

    Update Program.cs

--- SortedNumbers/SortedNumbers.Presenter/Program.cs
+++ SortedNumbers/SortedNumbers.Presenter/Program.cs
@@ -2,8 +2,10 @@
  * Terjes huskeliste:
  *  1: MVC
  *  2: MVP
- *  3: BIL
- *  4: JS
+ *  3: BIL - https://www.mooict.com/wpf-c-tutorial-create-a-simple-racing-game-in-visual-studio/
+ *           og https://github.com/GetAcademy/CarRacingOO (uten arv)
+ *           og https://github.com/GetAcademy/CarRacingOO2 (med arv)
+ *  4: JS - https://github.com/GetAcademy/SortedNumbersWithSum
  */
 
 using SortedNumbersPresenter;
