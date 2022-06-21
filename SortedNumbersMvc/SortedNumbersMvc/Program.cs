e9f9a299ddcba7531763aced2f505ca0c5b4274e
GET Academy <terje@getacademy.no>
2022-06-21 08:25:35 +0200

    x

--- SortedNumbersMvc/SortedNumbersMvc/Program.cs
+++ SortedNumbersMvc/SortedNumbersMvc/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SortedNumbersMvc;
+
+var numberList = new SortedNumbers();
+while (true)
+{
+    Console.Clear();
+    var numbers = numberList.Numbers;
+    if (numbers.Length > 0)
+    {
+        Console.WriteLine(" - " + string.Join("\n - ", numbers));
+    }
+
+    Console.WriteLine($"Summen er {numberList.Sum}");
+    Console.WriteLine("Kommandoer: \n - 1 => legge til tall\n - 2 => fjerne tall");
+    Console.Write("Hva vil du gjøre? ");
+    var command = Console.ReadLine();
+    if (command == "1")
+    {
+        Console.Write("Hvilket tall vil du legge til? ");
+        var numberStr = Console.ReadLine();
+        var number = Convert.ToInt32(numberStr);
+        numberList.Add(number);
+    } else if (command == "2")
+    {
+        Console.Write("Hvilket tall vil du fjerne? ");
+        var numberStr = Console.ReadLine();
+        var number = Convert.ToInt32(numberStr);
+        numberList.Remove(number);
+    }
+}
---
+++ SortedNumbersMvc/SortedNumbersMvc/SortedNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace SortedNumbersMvc
+{
+    internal class SortedNumbers
+    {
+        private List<int> _numbers;
+        public int[] Numbers => _numbers.ToArray();
+        //public int InputNumber { get; set; }
+        public int Sum => _numbers.Sum();
+
+        public SortedNumbers()
+        {
+            _numbers = new List<int>();
+        }
+
+        public void Add(int number)
+        {
+            var i = 0;
+            while (i < _numbers.Count && _numbers[i] < number)
+            {
+                i++;
+            }
+            _numbers.Insert(i, number);
+        }
+
+        public void Remove(int number)
+        {
+            _numbers.Remove(number);
+        }
+    }
+}
